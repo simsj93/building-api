4d7969511b52ae2747e87c144fe67716edf30208
Jake Sims <jake.jake.sims@gmail.com>
2022-03-28 11:33:53 -0500

    editted to match walkthrough

--- server/routes/chirps.js
+++ server/routes/chirps.js
@@ -1,23 +1,45 @@
 const express = require('express'); 
+const chirpstore = require('../chirpstore');
 const chirpsStore = require('../chirpstore'); 
 
 let router = express.Router(); 
 
 
 
-router.get('/:id?', (req, res) => {
-    let id = req.params.id
-    if (id) {
-        res.json(chirpsStore.GetChirp(id)); 
-    } else {
-    res.send(chirpsStore.GetChirps()); 
-    }
+router.get('/', (req, res) => {
+    
 }); 
+
+router.get('/id?', (req, res) => {
+    const id = req.params.id; 
+    if (id) {
+        const chirp = chirpsStore.GetChirp(id);
+        res.send(chirp);
+
+    } else {
+        const chirps = chirpsStore.GetChirps();
+        res.send(chirps); 
+    }
+})
+
 
 router.post('/', (req, res) => {
     chirpsStore.CreateChirp(req.body);
-    res.send(200); 
+    res.send('great jorb chirp posted'); 
 }); 
+
+router.delete('/:id', (req, res) => {
+  let id =  req.params.id;
+    chirpsStore.DeleteChirp(id); 
+    res.send('chirp deleted'); 
+}); 
+
+router.put('/:id', (req,res) => {
+    let id = req.params.id; 
+    chirpsStore.UpdateChirp(id, req.body); 
+    res.sendStatus(200); 
+});
+
 
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,10 @@
 const express = require('express'); 
 const chirpsRouter = require('./chirps');
 
-
 let router = express.Router(); 
 
 router.use('/chirps', chirpsRouter);
-router.use('/users', usersRouter); 
+
 
 module.exports = router; 
 
